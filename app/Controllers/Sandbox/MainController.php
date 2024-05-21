bcb913f6890fb135de8dd5ff350264ced715a88d
Guntur <guntur@asi-asiapacific.com>
2024-05-21 07:53:42 +0700

    update source model

--- app/Controllers/Sandbox/MainController.php
+++ app/Controllers/Sandbox/MainController.php
@@ -7,7 +7,7 @@ class MainController{
 
     public function home(){
         
-        return View::present('layout.page', [
+        return View::present('dashboard.modalbox', [
 			'current_page' => 'project',  
             //'dropdownassesor' => dropdown_by_json( 'assesment_roles.json'  , 'code', 'assessor_label' ,'04'  ),
 		]);
